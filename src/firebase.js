3207fd6b2cc8fb15c1c42d204c6d7c425b532aac
Monish <monishtyagi5770@gmail.com>
2023-11-03 00:14:29 +0530

    3nov23-1

--- src/firebase.js
+++ src/firebase.js
@@ -10,13 +10,13 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA2iz-fB1JFAKHAcNo9EhKKp15F_Vq6QXY",
-  authDomain: "ecom-37fa4.firebaseapp.com",
-  projectId: "ecom-37fa4",
-  storageBucket: "ecom-37fa4.appspot.com",
-  messagingSenderId: "805897434320",
-  appId: "1:805897434320:web:543a3d189dd84c604edad5",
-  measurementId: "G-W7BZ2P1RVQ"
+  apiKey: "AIzaSyB43V5iRwut8tVj7f-gT0ezjAGBKKIp7ac",
+  authDomain: "ecom-com.firebaseapp.com",
+  projectId: "ecom-com",
+  storageBucket: "ecom-com.appspot.com",
+  messagingSenderId: "173749008724",
+  appId: "1:173749008724:web:14c974e278b8e59caaf0c1",
+  measurementId: "G-9J2818ZD8S"
 };
 
 // Initialize Firebase
@@ -26,3 +26,11 @@ const auth = getAuth(app);
 const storage = getStorage(app);
 const db = getFirestore(app);
 export { auth, storage, db };
+
+// apiKey: "AIzaSyA2iz-fB1JFAKHAcNo9EhKKp15F_Vq6QXY",
+// authDomain: "ecom-37fa4.firebaseapp.com",
+// projectId: "ecom-37fa4",
+// storageBucket: "ecom-37fa4.appspot.com",
+// messagingSenderId: "805897434320",
+// appId: "1:805897434320:web:543a3d189dd84c604edad5",
+// measurementId: "G-W7BZ2P1RVQ"
