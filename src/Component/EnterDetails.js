129df5e4f8adf879bc4ef352d39ef777b844b426
Monish <monishtyagi5770@gmail.com>
2023-11-02 23:44:02 +0530

    2nov23

--- src/Component/EnterDetails.js
+++ src/Component/EnterDetails.js
@@ -36,13 +36,13 @@ const saveOrderInDB= async()=>{
     document.getElementById("COD").classList.remove("bg-drymeBlue");
     document.getElementById("COD").classList.add("bg-black");
 //   console.log("Save");
-    let common=new Date();
-    let date=common.getDate()+"-"+common.getMonth()+"-"+common.getFullYear();
-    // console.log("date ",date);
-    let time=common.getHours()+":"+common.getMinutes()+":"+common.getSeconds();
-    let Id=common.getFullYear()+common.getMonth()+common.getDate()+""+common.getHours()+common.getMinutes()+common.getSeconds()+"";
-    console.log("id "+Id);
-    let userEmailId=localStorage.getItem("email");
+let common=new Date();
+let date=common.getDate()+"-"+(common.getMonth()+1)+"-"+common.getFullYear();
+// console.log("date ",date);
+let time=common.getHours()+":"+common.getMinutes()+":"+common.getSeconds();
+let Id=common.getFullYear()+common.getMonth()+common.getDate()+""+common.getHours()+common.getMinutes()+common.getSeconds()+"";
+// console.log("id "+Id);
+let userEmailId=localStorage.getItem("email");
     // console.log("name ",name);
     // console.log("email",address);
     // console.log("phone ",cell)
